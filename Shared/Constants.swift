62fd5dc301314856dde47ab3a1691236f5a5d24e
Anmol Verma <anmol.verma4@gmail.com>
2020-12-07 09:29:19 +0530

    1. enabled macOS support 2. fixed color value which needs to have representation as 23 / 255

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -8,6 +8,6 @@
 import Foundation
 import SwiftUI
 
-struct Constants{
-    static let darkColor = Color.init(red: 23, green: 34, blue: 44, opacity: 0)
+public struct Constants{
+    static let darkColor = Color.init(red: 23 / 255, green: 34 / 255, blue: 44 / 255)
 }
